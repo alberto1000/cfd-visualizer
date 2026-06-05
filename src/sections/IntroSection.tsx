21f7bf51f1e399c873922aa12be700039472e01c
alberto1000 <alberto.lolli94@gmail.com>
2026-06-05 18:27:27 +0200

    changed resolution from 4/4 to 3/4

--- src/sections/IntroSection.tsx
+++ src/sections/IntroSection.tsx
@@ -75,7 +75,7 @@ export function IntroSection() {
             <VideoReveal
               src="/videos/3d-model.mp4"
               ariaLabel="Visualizzazione del dominio di calcolo"
-              aspectRatio="4/4"
+              aspectRatio="3/4"
               className="border border-cfd-cyan/20 shadow-glow"
             />
           </div>
